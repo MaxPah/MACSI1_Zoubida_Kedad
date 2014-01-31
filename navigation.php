9a85b7ac25539ef884baf72da87ae82a620a4601
idetox <ripman_55@hotmail.com>
2014-01-31 15:38:35 +0100

    ajout de l'affichage jalon / tache

--- navigation.php
+++ navigation.php
@@ -1,122 +1,199 @@
 <nav class="navbar navbar-default" role="navigation\>
-<div class="collapse navbar-collapse">
-	<ul class="nav navbar-nav">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-			</button>
-			<a class="navbar-brand" href="#">
-				<?php echo $nameProject; ?>
-			</a>
-		</div>
-		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Lots  <b class="caret"></b></a>
-			<ul class="dropdown-menu">
-				<?php
-					$isLot=false; //variable qui permet de voir si on a des lots existants
-					$sqlIdProject= "SELECT id_projet
-						FROM projet
-						WHERE nom = '$nameProject'";
-					$reqIdProject=mysql_query($sqlIdProject);
-					$idProjectArray= mysql_fetch_array($reqIdProject);
-					$idProject=$idProjectArray['id_projet'];
-					$sqlLot= "	SELECT nom
-						FROM lot
-						WHERE id_projet = '$idProject'";
-					$reqLot=mysql_query($sqlLot);
-					while($nomLot = mysql_fetch_array($reqLot)) {
-						echo "<li><a href=\"#\">Voir ".$nomLot['nom']."</a></li>";
-						$isLot=true;
-					}
-				?>
-				<li class="divider"></li>
-				<li class="dropdown-header">Action</li>
-				<li><a data-toggle="modal" href="#addLot">Ajouter Lot</a></li>
-				<li><a data-toggle="modal" href="#delLot" >Supprimer Lot</a></li>
-			</ul>
-		</li>
-		<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Phases  <b class="caret"></b></a>
-			<ul class="dropdown-menu">
-				<?php
-					$sqlIdProject= "	SELECT id_projet
-						FROM projet
-						WHERE nom = '$nameProject'";
-					$reqIdProject=mysql_query($sqlIdProject);
-					$idProjectArray= mysql_fetch_array($reqIdProject);
-					$idProject=$idProjectArray['id_projet'];
-					$sqlPhase= "	SELECT nom
-						FROM phase
-						WHERE id_projet = '$idProject'";
-					$reqPhase=mysql_query($sqlPhase);
-					while($nomPhase = mysql_fetch_array($reqPhase)) {
-						echo "<li><a href=\"#\">Voir ".$nomPhase['nom']."</a></li>";
-					}
-				?>
-				<li class="divider"></li>
-				<li class="dropdown-header">Action</li>
-				<li><a data-toggle="modal" href="#addPhase">Ajouter Phase</a></li>
-				<li><a data-toggle="modal" href="#delPhase" >Supprimer Phase</a></li>
-			</ul>
-		</li>
-		<?php
-			if($isLot) { // si on a des lots, on affiche les sous projets
-		?>
-				<li class="dropdown">
+	<div class="collapse navbar-collapse">
+		<ul class="nav navbar-nav">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+				</button>
+				<a class="navbar-brand" href="#">
+					<?php echo $nameProject; ?>
+				</a>
+			</div>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Lots  <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<?php
+						$estLot=false; //variable qui permet de voir si on a des lots existants
+						$estSousprojet=false; //variable qui permet de voir si on a des sousprojets existants
+						$sqlIdProject= "SELECT id_projet
+							FROM projet
+							WHERE nom = '$nameProject'";
+						$reqIdProject=mysql_query($sqlIdProject);
+						$idProjectArray= mysql_fetch_array($reqIdProject);
+						$idProject=$idProjectArray['id_projet'];
+						$sqlLot= "	SELECT nom
+							FROM lot
+							WHERE id_projet = '$idProject'";
+						$reqLot=mysql_query($sqlLot);
+						while($nomLot = mysql_fetch_array($reqLot)) {
+							echo "<li><a href=\"#\">Voir ".$nomLot['nom']."</a></li>";
+							$estLot=true;
+						}
+					?>
+					<li class="divider"></li>
+					<li class="dropdown-header">Action</li>
+					<li><a data-toggle="modal" href="#addLot">Ajouter Lot</a></li>
+					<li><a data-toggle="modal" href="#delLot" >Supprimer Lot</a></li>
+				</ul>
+			</li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Phases  <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<?php
+						$estPhase=false; //variable qui permet de voir si on a des phases existantes
+						$sqlIdProject= "	SELECT id_projet
+							FROM projet
+							WHERE nom = '$nameProject'";
+						$reqIdProject=mysql_query($sqlIdProject);
+						$idProjectArray= mysql_fetch_array($reqIdProject);
+						$idProject=$idProjectArray['id_projet'];
+						$sqlPhase= "	SELECT nom
+							FROM phase
+							WHERE id_projet = '$idProject'";
+						$reqPhase=mysql_query($sqlPhase);
+						while($nomPhase = mysql_fetch_array($reqPhase)) {
+							echo "<li><a href=\"#\">Voir ".$nomPhase['nom']."</a></li>";
+							$estPhase=true;
+						}
+					?>
+					<li class="divider"></li>
+					<li class="dropdown-header">Action</li>
+					<li><a data-toggle="modal" href="#addPhase">Ajouter Phase</a></li>
+					<li><a data-toggle="modal" href="#delPhase" >Supprimer Phase</a></li>
+				</ul>
+			</li>
+			<?php
+				if($estLot) { // si on a des lots, on affiche les sous projets
+			?>
+			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Sous-projet  <b class="caret"></b></a>
 				<ul class="dropdown-menu">
 					<?php
-					$sqlIdLot = "SELECT id_lot
-						FROM lot
-						WHERE id_projet='$idProject'";
-					$reqIdLot=mysql_query($sqlIdLot);
-					while($resIdLot=mysql_fetch_array($reqIdLot)) {
-						$idLot=$resIdLot['id_lot'];
-						$sqlNomLotTmp= "SELECT nom
+						$sqlIdLot = "SELECT id_lot
 							FROM lot
-							WHERE id_lot= '$idLot'";
-						$reqNomLotTmp=mysql_query($sqlNomLotTmp);
-						if($nomLotTmp = mysql_fetch_array($reqNomLotTmp)) {
-							echo "<li class=\"dropdown-header\">".$nomLotTmp['nom']."</li>";
-							echo "<li class=\"divider\"></li>";
-						}
-						$sqlSousProjet= "SELECT nom
-							FROM sousprojet
-							WHERE id_lot = '$idLot'";
-						$reqSousProjet=mysql_query($sqlSousProjet);
-						while($nomSousProjet = mysql_fetch_array($reqSousProjet)) {
-							echo "<li><a href=\"#\">Voir ".$nomSousProjet['nom']."</a></li>";
+							WHERE id_projet='$idProject'";
+						$reqIdLot=mysql_query($sqlIdLot);
+						while($resIdLot=mysql_fetch_array($reqIdLot)) {
+							$idLot=$resIdLot['id_lot'];
+							$sqlNomLotTmp= "SELECT nom
+								FROM lot
+								WHERE id_lot= '$idLot'";
+							$reqNomLotTmp=mysql_query($sqlNomLotTmp);
+							if($nomLotTmp = mysql_fetch_array($reqNomLotTmp)) {
+								echo "<li class=\"dropdown-header\">".$nomLotTmp['nom']."</li>";
+								echo "<li class=\"divider\"></li>";
 							}
-					}
-				?>
-				<li class="divider"></li>
-				<li class="dropdown-header">Action</li>
-				<li><a data-toggle="modal" href="#addSousProjet">Ajouter Sous-projet</a></li>
-				<li><a data-toggle="modal" href="#delSousProjet" >Supprimer Sous-projet</a></li>
-			</ul>
-		</li>
-		<?php }?>
-		<?php
-					/*<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Phases  <b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Voir Phases</a></li>
-							<li class="divider"></li>
-							<li class="dropdown-header">Action</li>
-							<li><a href="#">Ajouter Phase</a></li>
-							<li><a href="#">Supprimer Phase</a></li>
-						</ul>
-					</li>*/
-					/*<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Sous-Projets  <b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Voir Sous-Projets</a></li>
-							<li class="divider"></li>
-							<li class="dropdown-header">Action</li>
-							<li><a href="#">Ajouter Sous-Projet</a></li>
-							<li><a href="#">Supprimer Sous-Projet</a></li>
-						</ul>
-					</li>
+							$sqlSousProjet= "SELECT nom
+								FROM sousprojet
+								WHERE id_lot = '$idLot'";
+							$reqSousProjet=mysql_query($sqlSousProjet);
+							while($nomSousProjet = mysql_fetch_array($reqSousProjet)) {
+								echo "<li><a href=\"#\">Voir ".$nomSousProjet['nom']."</a></li>";
+								$estSousprojet=true;
+								}
+						}
+					?>
+					<li class="divider"></li>
+					<li class="dropdown-header">Action</li>
+					<li><a data-toggle="modal" href="#addSousProjet">Ajouter Sous-projet</a></li>
+					<li><a data-toggle="modal" href="#delSousProjet" >Supprimer Sous-projet</a></li>
+				</ul>
+			</li>
+			<?php }?>
+			<?php
+				if($estPhase) { // si on a des phases, on affiche les jalons
+			?>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Jalon  <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<?php
+						$sqlIdPhase = "SELECT id_phase
+							FROM phase
+							WHERE id_projet='$idProject'";
+						$reqIdPhase=mysql_query($sqlIdPhase);
+						while($resIdPhase=mysql_fetch_array($reqIdPhase)) {
+							$idPhase=$resIdPhase['id_phase'];
+							$sqlNomPhaseTmp= "SELECT nom
+								FROM phase
+								WHERE id_phase= '$idPhase'";
+							$reqNomPhaseTmp=mysql_query($sqlNomPhaseTmp);
+							if($nomPhaseTmp = mysql_fetch_array($reqNomPhaseTmp)) {
+								echo "<li class=\"dropdown-header\">".$nomPhaseTmp['nom']."</li>";
+								echo "<li class=\"divider\"></li>";
+							}
+							$sqlJalon= "SELECT nom
+								FROM jalon
+								WHERE id_phase = '$idPhase'";
+							$reqJalon=mysql_query($sqlJalon);
+							while($nomJalon = mysql_fetch_array($reqJalon)) {
+								echo "<li><a href=\"#\">Voir ".$nomJalon['nom']."</a></li>";
+								}
+						}
+					?>
+					<li class="divider"></li>
+					<li class="dropdown-header">Action</li>
+					<li><a data-toggle="modal" href="#addSousProjet">Ajouter Jalon</a></li>
+					<li><a data-toggle="modal" href="#delSousProjet" >Supprimer Jalon</a></li>
+				</ul>
+			</li>
+			<?php }?>
+			<?php
+				if($estPhase && $estSousprojet) { // si on a des phases, on affiche les jalons
+			?>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Tache  <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<?php
+						$sqlIdPhase = "SELECT id_phase
+								FROM phase
+								WHERE id_projet='$idProject'";
+						$sqlIdLot = "SELECT id_lot
+							FROM lot
+							WHERE id_projet='$idProject'";
+						$reqIdLot=mysql_query($sqlIdLot);
+						while($resIdLot=mysql_fetch_array($reqIdLot)) {
+							$idLot=$resIdLot['id_lot'];
+							$sqlNomSousprojetTmp= "SELECT nom ,id_sousprojet
+								FROM sousprojet
+								WHERE id_lot= '$idLot'";
+							$reqNomSousprojetTmp=mysql_query($sqlNomSousprojetTmp);
+							if($nomSousprojetTmp = mysql_fetch_array($reqNomSousprojetTmp)) {
+								$idSousprojet=$nomSousprojetTmp['id_sousprojet'];
+								echo "<li class=\"dropdown-header\">".$nomSousprojetTmp['nom'];
+								echo "<ul class =\"dropdown-header\">";
+							}
+							$reqIdPhase=mysql_query($sqlIdPhase);
+							while($resIdPhase=mysql_fetch_array($reqIdPhase)) {
+								$idPhase=$resIdPhase['id_phase'];
+								$sqlNomPhaseTmp= "SELECT nom
+									FROM phase
+									WHERE id_phase= '$idPhase'";
+								$reqNomPhaseTmp=mysql_query($sqlNomPhaseTmp);
+								if($nomPhaseTmp = mysql_fetch_array($reqNomPhaseTmp)) {
+									echo "<li class=\"dropdown-header\">".$nomPhaseTmp['nom']."</li>";
+									echo "<li class=\"divider\"></li>";
+								}
+								$sqlTache= "SELECT nom
+									FROM tache
+									WHERE id_phase = '$idPhase'
+									AND id_sousprojet = '$idSousprojet'";
+								$reqTache=mysql_query($sqlTache);
+								while($nomTache = mysql_fetch_array($reqTache)) {
+									echo "<li><a href=\"#\">Voir ".$nomTache['nom']."</a></li>";
+								}
+							}
+						}
+						echo "</li></ul>";						
+					?>
+					<li class="divider"></li>
+					<li class="dropdown-header">Action</li>
+					<li><a data-toggle="modal" href="#addSousProjet">Ajouter Tache</a></li>
+					<li><a data-toggle="modal" href="#delSousProjet" >Supprimer Tache</a></li>
+				</ul>
+			</li>
+			<?php }?>
+			<?php
+					/*
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Taches  <b class="caret"></b></a>
 						<ul class="dropdown-menu">
@@ -125,16 +202,6 @@
 							<li class="dropdown-header">Action</li>
 							<li><a href="#">Ajouter Tache</a></li>
 							<li><a href="#">Supprimer Tache</a></li>
-						</ul>
-					</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Jalons  <b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Voir Jalons</a></li>
-							<li class="divider"></li>
-							<li class="dropdown-header">Action</li>
-							<li><a href="#">Ajouter Jalon</a></li>
-							<li><a href="#">Supprimer Jalon</a></li>
 						</ul>
 					</li>
 					
