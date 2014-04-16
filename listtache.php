e10935332f169429d77ade4e3c4ee178ac89ec53
iTeex <pierre_lizet@hotmail.fr>
2014-04-16 14:41:37 +0200

    Liste tâche
    On ne sélectionne que les tâches du projet en cours

--- listtache.php
+++ listtache.php
@@ -1,18 +1,27 @@
 <ul class="list-group">
   <?php 
+  $sqlIdProjet = ' SELECT id_projet
+  					FROM projet
+  					WHERE nom = "'.$nameProject.'"';
+  					
+  	$reqIdProjet = mysql_query($sqlIdProjet) or die('Erreur query : '.mysql_error());
+  	$resIdProjet = mysql_fetch_array($reqIdProjet) or die('Erreur result : '.mysql_error());
+  	$idProjet = $resIdProjet['id_projet'];
   
-	$sqlIdTache = "SELECT t.nom as nomTache, s.nom as nomSP, p.nom as nomP
+	$sqlTache = "SELECT t.nom as nomTache, s.nom as nomSP, p.nom as nomP
 					FROM tache t,sousprojet s, phase p
 					WHERE t.id_sousprojet=s.id_sousprojet
-					AND t.id_phase= p.id_phase";
-	$reqIdTache=mysql_query($sqlIdTache);
+					AND t.id_phase= p.id_phase
+					AND p.id_projet = $idProjet";
+					
+	$reqTache=mysql_query($sqlTache) or die('Erreur query 2 : '.mysql_error());
 						
-	while ($resIdTache= mysql_fetch_array($reqIdTache)) {
+	while ($resTache= mysql_fetch_array($reqTache)) {
 		echo"
 		<li class=\"list-group-item\">";
-				echo "<span class=\"badge\">Phase : ".$resIdTache['nomP']."</span>";
-				echo "<span class=\"badge\">Sous Projet : ".$resIdTache['nomSP']."</span>";
-			echo $resIdTache['nomTache'];"
+				echo "<span class=\"badge\">Phase : ".$resTache['nomP']."</span>";
+				echo "<span class=\"badge\">Sous Projet : ".$resTache['nomSP']."</span>";
+			echo $resTache['nomTache'];"
 		</li>";
 		}
 ?>
