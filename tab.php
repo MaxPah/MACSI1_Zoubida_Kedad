fcc89e1415c8817d7aff2fea13bc2be35618b98a
Pahud Maxime <maximepahud@live.fr>
2014-04-22 01:23:35 +0200

    Amelioration du tableau de bord
    Ajout des dates prev du % du projet

--- tab.php
+++ tab.php
@@ -40,6 +40,8 @@
 			?>
 		</div>
 		<ul class="list-group">
+		
+		
 		<?php
 		/* BASE */
 			$sqlp = 'SELECT *
@@ -48,11 +50,40 @@
 			$reqp = mysql_query($sqlp) or die('Erreur requete 2 : '.mysql_error());
 			while($resp = mysql_fetch_array($reqp))					
 			{
-				echo "<li class=\"list-group-item\"> <u><strong><i>Enveloppe budg&eacute;taire </i></strong></u> : ".$resp['enveloppe_budg']."</li>";
+				echo "<li class=\"list-group-item\"> <u><strong><i>Enveloppe budg&eacute;taire pr&eacute;visionnelle</i></strong></u> : ".$resp['enveloppe_budg']."</li>";
 				echo "<li class=\"list-group-item\"> <u><strong><i>D&eacute;scription </i></strong></u> : ".$resp['description']."</li>";
 				echo "<li class=\"list-group-item\"> <u><strong><i>Date de fin pr&eacute;visionnelle </i></strong></u> : ".$resp['datefin']."</li>";
 			}
 		?>
+		<?php
+			$sqlp = 'SELECT datefin,datedeb
+						 FROM projet
+						 WHERE id_projet ="'.$idP.'"';
+				$reqp = mysql_query($sqlp) or die('Erreur requete 2 : '.mysql_error());
+				while($resp = mysql_fetch_array($reqp))					
+					{
+					$datefin = $resp['datefin'];
+					$datedeb = $resp['datedeb'];
+					}
+				$datejour =date('Y-m-d');
+				
+				$sqlp2 = 'SELECT DATEDIFF(\''.$datefin.'\',\''.$datedeb.'\') as deb';
+				$reqp2 = mysql_query($sqlp2) or die('Erreur requete 2 : '.mysql_error());
+				while($resp2 = mysql_fetch_array($reqp2))					
+					$a = $resp2['deb'];
+					
+				$sqlp = 'SELECT DATEDIFF(\''.$datefin.'\',\''.$datejour.'\') as this';
+				$reqp = mysql_query($sqlp) or die('Erreur requete 2 : '.mysql_error());
+				while($resp = mysql_fetch_array($reqp))					
+				{
+					$moy = $resp['this']*100 / $a ;
+					$moy =number_format($moy);
+					echo "<li class=\"list-group-item\"> <u><strong><i>Fin du projet dans  </i></strong></u> : ".$resp['this']."  jours (approx)</li>";
+					echo "<li class=\"list-group-item\"> <u><strong><i>Projet effectu&eacute; &agrave;  </i></strong></u> : ".$moy." % (approx)</li>";
+				}
+		
+		?>
+		
 		
 		<?php
 		/* LOT */
